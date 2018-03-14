28fd1a3d599bfd6384b42c9a57ea12daecd67b71
AndreyKurennoy <19ofis96@mail.ru>
2018-03-14 16:34:32 +0200

    api collection request

---
+++ app/Http/Requests/CollectionRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CollectionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'warranty' => 'required',
+            'weight' => 'required',
+            'size' => 'required',
+            'length_of_sheets' => 'required',
+            'quantity_of_sheets' => 'required',
+            'quantity_of_boxes' => 'required',
+            'protrusion' => 'required',
+            'wind_min' => 'required',
+            'wind_max' => 'required',
+            'angle' => 'required',
+            'description' => 'required'
+        ];
+    }
+}
